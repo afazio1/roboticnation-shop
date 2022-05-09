c5c636921ea30ebd975cba41f01596375da540c2
afazio1 <alexa.fazio04@gmail.com>
2022-05-09 10:43:57 -0400

    partials and product routes

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # home
     path('', views.index, name='index'),
+
+    # products
     path('products/', views.products),
-    path('products/<str:id>/', views.product),
+    path('products/<str:category>/', views.category),
+    path('products/<str:category>/<str:id>', views.product)
     
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-
+# Product Related
 def index(request):
     return render(request, "index.html", {"name": "Alexa"})
 
 def products(request):
-    return render(request, "product_routes/products.html")
+    return render(request, "product_routes/categories.html")
 
-def product(request, id):
-    return render(request, "product_routes/product.html", {"id": id})
+def category(request, category):
+    return render(request, "product_routes/category.html", {"category" : category})
+
+def product(request, category, id):
+    return render(request, "product_routes/product.html", {"category" : category, "id": id})

7d52fe208828fd12221bf119186a9b57db5f0dbb
afazio1 <alexa.fazio04@gmail.com>
2022-05-08 22:44:54 -0400

    society

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
+    path('store/', include('store.urls')),
     path('playground/', include('playground.urls'))
 ]

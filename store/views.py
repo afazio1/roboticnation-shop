d2887015a248c57d774690b701fc81cf2f3514c2
afazio1 <alexa.fazio04@gmail.com>
2022-05-09 14:28:54 -0400

    db connection code

--- store/views.py
+++ store/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+# MongoDB Connection
+from pymongo import MongoClient
+# client = MongoClient('mongodb+srv://<username>:<password>@<atlas cluster>/<myFirstDatabase>?retryWrites=true&w=majority')
+# db = client['shop']
+
+
 # Product Related
 def index(request):
     return render(request, "index.html", {"name": "Alexa"})
 
 def products(request):
-    return render(request, "product_routes/categories.html")
+    if request.method == "GET":
+        return render(request, "product_routes/categories.html")
 
 def category(request, category):
-    return render(request, "product_routes/category.html", {"category" : category})
+    if request.method == "GET":
+        return render(request, "product_routes/category.html", {"category" : category})
 
 def product(request, category, id):
-    return render(request, "product_routes/product.html", {"category" : category, "id": id})
+    if request.method == "GET":
+        return render(request, "product_routes/product.html", {"category" : category, "id": id})

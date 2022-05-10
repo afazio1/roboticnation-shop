1e70511980b0353fc8fcbada7c077ba24cb17047
Sebastian-Joseph <theclimax25@gmail.com>
2022-05-10 09:11:55 -0400

    connected database

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+# MongoDB Connection
+import pymongo
+from pymongo import MongoClient
+from credentials import user, pswd
+# cluster = MongoClient('mongodb+srv://<user>:<pswd>@robonatty-cluster.cykzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+# db = client['shop']
 
-def say_hello(request):
-    return render(request, "hello.html", {"name": "Alexa"})
+
+# Product Related
+def index(request):
+    return render(request, "index.html", {"name": "Alexa"})
+
+def products(request):
+    if request.method == "GET":
+        return render(request, "product_routes/categories.html")
+
+def category(request, category):
+    if request.method == "GET":
+        return render(request, "product_routes/category.html", {"category" : category})
+
+def product(request, category, id):
+    if request.method == "GET":
+        return render(request, "product_routes/product.html", {"category" : category, "id": id})
